cacd8cb55b1d4ffc16906be6425dc3f0cd7aba8d
Ventsislav Todorov <todorov.ventsi@gmail.com>
2023-06-12 17:17:31 +0300

    working with dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,19 +81,32 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = '';
+const daysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
+  console.log(acc.movements);
+
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+        <div class="movements__date">${displayDate}</div>      
         <div class="movements__value">${mov.toFixed(0)}€</div>
       </div>
     `;
@@ -142,7 +155,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,6 +167,11 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// FAKE LOGIN - TO BE DELETED AFTER
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -169,6 +187,12 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, '0');
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const year = now.getFullYear();
+    labelDate.textContent = `${day}/${month}/${year}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -197,6 +221,10 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // Add date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -210,6 +238,9 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    // Add date
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -243,7 +274,7 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
@@ -255,43 +286,120 @@ btnSort.addEventListener('click', function (e) {
 // Binary Base 2: 0 1
 
 // Parsing
+/*
 console.log(Number.parseInt('30px', 10)); // parse the string to decimal number 30
 console.log(Number.parseInt('e23', 10)); // doesn't work (NaN)
 console.log(Number.parseInt('0101010', 2)); // parse the binary number
 console.log(Number.parseFloat('2.5rem')); // parse the floating point number
+*/
 
 // checks if a value is NaN
+/*
 console.log(Number.isNaN('20')); // false
 console.log(Number.isNaN(20)); // false
 console.log(Number.isNaN(20 / 0)); // false
 console.log(Number.isNaN(+'20X')); // true
+*/
 
 // checks if a value is number
+/*
 console.log(Number.isFinite(20)); // true
 console.log(isFinite('20')); // true (Converts the value to a number before evaluating)
 console.log(Number.isFinite('20')); // false (Does not converts the value to a number before evaluating)
+*/
 
 // 1. MATH AND ROUNDING
-
+/*
 console.log(Math.sqrt(25)); // 5
 console.log(Math.max(5, 16, '89')); // 89 (Does type converstion, but not parsing)
 console.log(Math.max(5, 16, '89', '100p')); // Nan (Does type converstion, but not parsing)
+*/
 
 // Random number between 2 values
+/*
 const randomInt = (min, max) =>
   Math.trunc(Math.random() * (max - min) + 1) + min;
 
 console.log(randomInt(10, 20));
+*/
 
 // Rounding integers - all the methods does type conversion
+/*
 console.log(Math.trunc(1.21321331)); // 1 (Removes any decilamal part)
 console.log(Math.round(23.3)); // 23 (Rounds to the nearest integer)
 console.log(Math.round(23.9)); // 24 (Rounds to the nearest integer)
 console.log(Math.ceil(23.3)); // 24 (Rounds to the highest integer)
 console.log(Math.floor(23.3)); // 23 (Rounds to the lowest integer)
+*/
 
 // Rounding decimals
+/*
 console.log((2.3).toFixed(0)); // '2'
 console.log((2.3241414).toFixed(1)); // '2.3'
 console.log((2.3241414).toFixed(2)); // '2.32'
 console.log((2.3).toFixed(3)); // '2.300'
+*/
+
+// Remainder operator %
+// console.log(5 % 2); // 1
+
+// Set every even row backgraund color:
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+//     if (i % 2 === 0) {
+//       row.style.backgroundColor = 'orangered';
+//     }
+//   });
+// });
+
+// Numeric separetors
+/*
+const diameter = 287_640_000_000;
+console.log(diameter);
+*/
+
+// Work with BIGINT - used to work with number biggest from MAX_SAFE_INTEGERS, BigInt and regular numbers are not compatible
+/*
+console.log(2 ** 53 - 1); // the biggest number with which is safely to work in JS
+console.log(Number.MAX_SAFE_INTEGER); // the biggest number with which is safely to work in JS
+console.log(9292929292929292929922132131231n);
+console.log(BigInt(9292929292929292929922132131231n));
+*/
+
+// Creating dates
+/*
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Mon Jun 12 2023 15:19:39')); // Parsing from a string
+console.log(new Date('December 24, 2015')); // Parsing from a string
+console.log(new Date(account1.movementsDates[0])); // Parsing from an object property
+console.log(new Date(2037, 10, 19, 15, 23, 5)); // Parsing from an numbers (month is zero-based)
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); // Timestamp for day number 3!!!
+ */
+
+// Date object methods
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear()); // Retrieve the year out of the date object
+console.log(future.getMonth()); // Retrieve the month out of the date object
+console.log(future.getDate()); // Retrieve the date out of the date object
+console.log(future.getDay()); // Retrieve the day of the week out of the date object
+console.log(future.getTime()); // Retrieve the time stamp
+
+console.log(new Date(future.getTime())); // Creating date from the timestamp!!!
+ */
+
+// Operations with dates
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(Number(future)); // 'future' date's timestamp
+
+const daysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24); // Coverting mileseconds to days
+
+console.log(daysPassed(new Date(2022, 3, 14), new Date(2022, 3, 10)));
+*/
